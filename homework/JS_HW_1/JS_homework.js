c0367b4595fb07c4917290bdb6d1db94ab087e11
B0ond <amiram19@yandex.ru>
2022-04-19 00:24:00 +0300

    js hm 1

---
+++ homework/JS_HW_1/JS_homework.js
@@ -0,0 +1,25 @@
+//ex_2_hm_js_1
+let item_1;
+item_1 = 5;
+console.log("item_1 = " + item_1);
+
+//ex_2_hm_js_1
+let item_2;
+item_2 = 3;
+console.log("item_2 = " + item_2);
+
+//ex_3_hm_js_1
+let item_3;
+item_3 = item_1 + item_2;
+console.log("item_3 = " + item_3);
+
+//ex_4_hm_js_1
+let item_4;
+item_4 = "Yolochka";
+console.log("item_4 = " + item_4);
+
+//ex_5_hm_js_1                    сложение
+console.log(item_3 + item_4)
+
+//ex_6_hm_js_1                     тут вопрос в умножении
+console.log(item_3 * item_4)
