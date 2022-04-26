75f8c6608fe47e4307fc9bc05f71c610d3903677
Amir <amiram19@yandex.ru>
2022-04-26 17:30:49 +0300

    ch js_hw_1_*

--- homework/JS_HW_1/JS_homework_1_ultra.js
+++ homework/JS_HW_1/JS_homework_1_ultra.js
@@ -32,27 +32,27 @@
 
 
 
-const checkAge = function (age) {
-    const age_min = 18;
-    const age_max = 60;
-    if (typeof (age) == 'number' || typeof (age) == 'string') {  
-        if (age < age_min) {
-            console.log("You don’t have access cause your age is", age, "It’s less then ")
-        } else if (age >= age_min && age < age_max) {
-            console.log("Welcome !")
-        } else if (age > age_max) {
-            console.log("Keep calm and look Culture channe !")
-        } else {
-            console.log("Technical work")
-        }
-    }
-    else {
-        console.log(age, "is not a number")
-    }
+// const checkAge = function (age) {
+//     const age_min = 18;
+//     const age_max = 60;
+//     if (typeof (age) == 'number' || typeof (age) == 'string') {  
+//         if (age < age_min) {
+//             console.log("You don’t have access cause your age is", age, "It’s less then ")
+//         } else if (age >= age_min && age < age_max) {
+//             console.log("Welcome !")
+//         } else if (age > age_max) {
+//             console.log("Keep calm and look Culture channe !")
+//         } else {
+//             console.log("Technical work")
+//         }
+//     }
+//     else {
+//         console.log(age, "is not a number")
+//     }
 
-}
+// }
 
-checkAge('12')
+// checkAge('12')
 
 
 
@@ -78,3 +78,35 @@ checkAge('12')
 // }
 
 // checkAge(prompt("Enter your age please: ", ''))
+
+
+
+
+const checkAge = function (age) {
+    const age_min = 18;
+    const age_max = 60;
+    age = parseInt(age)
+    // console.log(typeof(age), age)
+    if (typeof (age) == 'number' || typeof (age) == 'string') {
+        if (isNaN(age)) {
+            console.log(age, "is not a number")
+        }
+        else if (age < age_min) {
+            console.log("You don’t have access cause your age is", age, "It’s less then ")
+        }
+         else if (age >= age_min && age < age_max) {
+            console.log("Welcome !")
+        } else if (age > age_max) {
+            console.log("Keep calm and look Culture channe !")
+        } else {
+            console.log("Technical work")
+        }
+    }
+    
+    else {
+        console.log(age, "is not a number")
+    }
+
+}
+
+checkAge(19)
