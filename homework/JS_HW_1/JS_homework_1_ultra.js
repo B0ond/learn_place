d3a571ff71e6a5680e6202b1e37e5bd130a047e7
B0ond <amiram19@yandex.ru>
2022-04-26 23:14:07 +0300

    hw js_1_ultra

--- homework/JS_HW_1/JS_homework_1_ultra.js
+++ homework/JS_HW_1/JS_homework_1_ultra.js
@@ -57,56 +57,63 @@
 
 
 
+const checkAge = function (age) {
+    const age_min = 18;
+    const age_max = 60;
+    age = parseInt(age);
+    // console.log(typeof(age), age)
+    if (typeof (age) == 'number' || typeof (age) == 'string') {
+        if (isNaN(age)) {
+            alert(" is not a number")
+        }
+        else if (age < age_min) {
+            alert("You don’t have access cause your age is " + age + " It’s less then ")
+        }
+         else if (age >= age_min && age < age_max) {
+            alert("Welcome !")
+        } else if (age > age_max) {
+            alert("Keep calm and look Culture channe !")
+        } else {
+            alert("Technical work")
+        }
+    }
+    
+    else {
+        alert(age + " is not a number")
+    }
+
+}
+
+checkAge(prompt('Enter your age please:'), '')
+
+
+
+
 // const checkAge = function (age) {
 //     const age_min = 18;
 //     const age_max = 60;
-//     if (typeof (age) == 'number' || age == 'string') {  
-//         if (age < age_min) {
-//             alert("You don’t have access cause your age is", age, "It’s less then ")
-//         } else if (age >= age_min && age < age_max) {
-//             alert("Welcome !")
+//     age = parseInt(age);
+//     // console.log(typeof(age), age)
+//     if (typeof (age) == 'number' || typeof (age) == 'string') {
+//         if (isNaN(age)) {
+//             console.log(age, "is not a number")
+//         }
+//         else if (age < age_min) {
+//             console.log("You don’t have access cause your age is", age, "It’s less then ")
+//         }
+//          else if (age >= age_min && age < age_max) {
+//             console.log("Welcome !")
 //         } else if (age > age_max) {
-//             alert("Keep calm and look Culture channe !")
+//             console.log("Keep calm and look Culture channe !")
 //         } else {
-//             alert("Technical work")
+//             console.log("Technical work")
 //         }
 //     }
+    
 //     else {
 //         console.log(age, "is not a number")
 //     }
 
 // }
 
-// checkAge(prompt("Enter your age please: ", ''))
-
-
-
-
-const checkAge = function (age) {
-    const age_min = 18;
-    const age_max = 60;
-    age = parseInt(age)
-    // console.log(typeof(age), age)
-    if (typeof (age) == 'number' || typeof (age) == 'string') {
-        if (isNaN(age)) {
-            console.log(age, "is not a number")
-        }
-        else if (age < age_min) {
-            console.log("You don’t have access cause your age is", age, "It’s less then ")
-        }
-         else if (age >= age_min && age < age_max) {
-            console.log("Welcome !")
-        } else if (age > age_max) {
-            console.log("Keep calm and look Culture channe !")
-        } else {
-            console.log("Technical work")
-        }
-    }
-    
-    else {
-        console.log(age, "is not a number")
-    }
-
-}
-
-checkAge(19)
+// checkAge('18dfg')
---
+++ poligon/poligon_js.js
@@ -0,0 +1,65 @@
+// let f1 = () => console.log('Hellow f1');
+// // f1()
+
+// let fl11 = 11;
+// let fl22 = 12;
+
+// let func1 = (fl1, fl2) => {console.log('Hellow func1 ', fl1 + fl2)};
+// func1(fl11, fl22)
+
+// let func2 = (fl11 > 10) ?
+//     (fl1, fl2) => {console.log('Hellow func2 1', fl1 + fl2)}:
+//     (fl1, fl2) => {console.log('Hellow func2 2', fl1 - fl2)};
+    
+
+// func2(fl11, fl22)
+
+
+
+// function Yolochka(){}
+
+// Yolochka.prototype.method1 = function(){
+//     console.log('method1')
+// }
+
+// Yolochka.prototype.method2 = function(n1, n2){
+//     console.log('method2', n1 + n2)
+// }
+
+// Yolochka.prototype.method3 = () => {
+//     console.log('method3')
+// }
+
+// let forest = new Yolochka()
+
+// forest.method1()
+// forest.method2(100, 200)
+// forest.method1()
+
+
+
+// let count = 0
+// let countStop = 10
+
+// while (count < 10){
+//     console.log('Hellow!!! --', count)
+//     count ++
+// }
+
+
+let countStop = 10
+let count = 0
+let names = ['Kate', 'Alex', 'Ivan', 'Ignat', 'Andrey', 'Vika']
+
+let obj1 = {
+    'n1': '1111',
+    'n1': '2222',
+    'n1': '33333',
+    'n1': '4444',
+    'n1': '5555',
+    'n1': '6666'
+}
+
+for (let item in names){
+    console.log('name= ', names[item], item)
+}
