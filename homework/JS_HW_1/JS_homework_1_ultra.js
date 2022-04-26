623cad2bcea379af94fe871a00271241f1151baa
Amir <amiram19@yandex.ru>
2022-04-26 16:16:44 +0300

    ch js_hw_1_*

--- homework/JS_HW_1/JS_homework_1_ultra.js
+++ homework/JS_HW_1/JS_homework_1_ultra.js
@@ -12,14 +12,6 @@
 
 // a()
 
-// const freindNames = {
-//     mans: ['Igor', 'Alex', 'Ruslan'],
-//     girls: ['Regina', 'Sasha']
-// }
-// const freindNames2 = Object.assign({}, freindNames)
-// freindNames2.girlFriends = ['Sasha', 'Prati']
-// console.log(freindNames)
-// console.log(freindNames2)
 
 
 
@@ -43,7 +35,7 @@
 const checkAge = function (age) {
     const age_min = 18;
     const age_max = 60;
-    if (typeof (age) == 'number') {  
+    if (typeof (age) == 'number' || typeof (age) == 'string') {  
         if (age < age_min) {
             console.log("You don’t have access cause your age is", age, "It’s less then ")
         } else if (age >= age_min && age < age_max) {
@@ -60,4 +52,29 @@ const checkAge = function (age) {
 
 }
 
-checkAge('asf')
+checkAge('12')
+
+
+
+
+// const checkAge = function (age) {
+//     const age_min = 18;
+//     const age_max = 60;
+//     if (typeof (age) == 'number' || age == 'string') {  
+//         if (age < age_min) {
+//             alert("You don’t have access cause your age is", age, "It’s less then ")
+//         } else if (age >= age_min && age < age_max) {
+//             alert("Welcome !")
+//         } else if (age > age_max) {
+//             alert("Keep calm and look Culture channe !")
+//         } else {
+//             alert("Technical work")
+//         }
+//     }
+//     else {
+//         console.log(age, "is not a number")
+//     }
+
+// }
+
+// checkAge(prompt("Enter your age please: ", ''))
