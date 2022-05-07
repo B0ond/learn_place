058e594d183abdf1dc53b493c38a7e053870a94a
B0ond <amiram19@yandex.ru>
2022-05-07 13:19:38 +0300

    js2

--- homework/JS_HW_2/js_hw_2.js
+++ homework/JS_HW_2/js_hw_2.js
@@ -22,28 +22,28 @@
 
 // login("34dDs@");
 
-//Through browser
-// function login(check) {
-//   if (typeof check == "string") {
-//     if (check.length <= 4) {
-//       alert("Short name. Enter from 5 characters");
-//     } else if (check.length >= 65) {
-//       alert("Too long name");
-//     } else if (!check.match(/[a-zа-яё]/)) {
-//       alert("Enter letters");
-//     } else if (!check.match(/[A-ZА-ЯЁ]/)) {
-//       alert("Enter Bigletters");
-//     } else if (!check.match(/[0-9]/)) {
-//       alert("Enter numbers");
-//     } else if (!check.match(/[@]/)) {
-//       alert("Enter @");
-//     } else {
-//       alert("Thank's, it's ok");
-//     }
-//   } else {
-//     alert("Enter string");
-//   }
-// }
+Through browser
+function login(check) {
+  if (typeof check == "string") {
+    if (check.length <= 4) {
+      alert("Short name. Enter from 5 characters");
+    } else if (check.length >= 65) {
+      alert("Too long name");
+    } else if (!check.match(/[a-zа-яё]/)) {
+      alert("Enter letters");
+    } else if (!check.match(/[A-ZА-ЯЁ]/)) {
+      alert("Enter Bigletters");
+    } else if (!check.match(/[0-9]/)) {
+      alert("Enter numbers");
+    } else if (!check.match(/[@]/)) {
+      alert("Enter @");
+    } else {
+      alert("Thank's, it's ok");
+    }
+  } else {
+    alert("Enter string");
+  }
+}
 
 // login(prompt("Chek me", ""));
 
@@ -53,18 +53,3 @@
 //   return str.replace(/([A-Z])/g, ' $1')
 // }
 // console.log(removeCc('сamelCase'))
-
-function ask(question, yes, no) {
-  if (confirm(question)) yes();
-  else no();
-}
-
-ask(
-  "Вы согласны?",
-  function () {
-    alert("Вы согласились.");
-  },
-  function () {
-    alert("Вы отменили выполнение.");
-  }
-);
